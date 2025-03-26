c1009c12304764c6a11cab1157a550d1aa5b9332
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-26 13:59:45 +0530

    installed tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import "./App.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Slider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 function App() {
-  return <MantineProvider>Hello World</MantineProvider>;
+  return (
+    <MantineProvider>
+      <h1 className="text-3xl text-blue-800">Hello world!</h1>
+      <Slider
+        color="blue"
+        marks={[
+          { value: 20, label: "20%" },
+          { value: 50, label: "50%" },
+          { value: 80, label: "80%" },
+        ]}
+      />
+    </MantineProvider>
+  );
 }
 
 export default App;
