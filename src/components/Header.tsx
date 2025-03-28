0474983f137e2a42f5f71e8dce7d4c148398bbfb
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-28 16:17:13 +0530

    Header notification completed UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import { Avatar } from "@mantine/core";
+import { Avatar, Indicator } from "@mantine/core";
 import { IconAnchor, IconBell, IconSettings } from "@tabler/icons-react";
 
 const Header = () => {
   return (
-    <div className="w-full bg-mine-shaft-950 p-6 text-white h-28 flex justify-between items-center">
+    <div className="w-full bg-mine-shaft-950 p-6 text-white h-20 flex justify-between items-center">
       <div className="flex gap-2 items-center text-bright-sun-400">
         <IconAnchor className="h-8 w-8" stroke={2.5} />
         <div className="text-3xl font-semibold">JobHook</div>
@@ -19,8 +19,14 @@ const Header = () => {
           <div>Sanskar</div>
           <Avatar src="/avatar.png" alt="it's me" />
         </div>
-        <IconSettings />
-        <IconBell />
+        <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+          <IconSettings stroke={1.5} />
+        </div>
+        <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+          <Indicator offset={6} color="bright-sun.4" size={8} processing>
+            <IconBell stroke={1.5} />
+          </Indicator>
+        </div>
       </div>
     </div>
   );
