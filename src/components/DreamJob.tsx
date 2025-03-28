b3851b9717c333a3b2789707aab917de5ab17fc6
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-28 17:04:27 +0530

    Landing Page UI

--- src/components/DreamJob.tsx
+++ src/components/DreamJob.tsx
@@ -1,5 +1,23 @@
 const DreamJob = () => {
-  return <div>DreamJob</div>;
+  return (
+    <div className="flex items-center px-16">
+      <div className="flex flex-col w-[45%] gap-3">
+        <div className="text-6xl font-bold leading-tight text-mine-shaft-100 [&>span]:text-bright-sun-400">
+          Find your <span>dream </span>
+          <span>job</span> with us
+        </div>
+        <div className="text-lg text-mine-shaft-100">
+          Good life begins with a good company. Start explore thousands of jobs
+          in one place.
+        </div>
+      </div>
+      <div className="w-[55%] flex items-center justify-center">
+        <div className="w-[30rem]">
+          <img src="/Boy.png" alt="Boy" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DreamJob;
