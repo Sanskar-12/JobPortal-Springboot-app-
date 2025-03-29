0c2f2d80461343882252070b20c0af0a62b5a425
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-29 11:49:35 +0530

    Companies COmponent Completed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header";
+import Companies from "../components/LandingPage/Companies";
 import DreamJob from "../components/LandingPage/DreamJob";
 
 const HomePage = () => {
   return (
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
-      <Header />;
+      <Header />
       <DreamJob />
+      <Companies />
     </div>
   );
 };
