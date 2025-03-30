7ede81c60679a8e3ab7c3eb23f95e2404e4e8ebb
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-30 17:42:19 +0530

    Working Ui half completed

--- src/components/LandingPage/Working.tsx
+++ src/components/LandingPage/Working.tsx
@@ -1,3 +1,6 @@
+import { Avatar } from "@mantine/core";
+import { work } from "../../Data/Data";
+
 const Working = () => {
   return (
     <div className="mt-20 pb-5">
@@ -7,23 +10,35 @@ const Working = () => {
       <div className="text-lg mb-10 mx-auto text-mine-shaft-300 text-center w-1/2">
         Effortlessly navigate through the process and land your dream job.
       </div>
-      <div>
+      <div className="flex px-16 justify-around items-center">
         <div>
           <img className="w-[30rem]" src="/Working/Girl.png" alt="Girl" />
-        </div>
-        <div>
-          <div className="flex items-center">
-            <div>
-              <img src="/Working/Build your resume.png" alt="Build" />
+          <div>
+            <Avatar className="!h-16 !w-16" src={"/avatar1.png"} alt="Its me" />
+            <div className="text-sm font-semibold text-mine-shaft-200 text-center">
+              Complete your profile
             </div>
-            <div>
-              <div>Build your resume</div>
+            <div className="text-xs text-mine-shaft-300">70% Completed</div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-10">
+          {work.map((item, index) => (
+            <div className="flex items-center gap-4" key={index}>
+              <div className="p-2.5 bg-bright-sun-300 rounded-full">
+                <img
+                  src={`/Working/${item.name}.png`}
+                  alt={item.name}
+                  className="h-12 w-12"
+                />
+              </div>
               <div>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
-                dolor? Consectetur{" "}
+                <div className="text-mine-shaft-200 text-xl font-semibold">
+                  {item.name}
+                </div>
+                <div className="text-mine-shaft-300">{item.desc}</div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
