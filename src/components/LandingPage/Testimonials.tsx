5d0960be1a2af6448316dab82086c128c9a705f1
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-31 11:30:54 +0530

    Testimonials Ui done

--- src/components/LandingPage/Testimonials.tsx
+++ src/components/LandingPage/Testimonials.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Rating } from "@mantine/core";
+import { testimonials } from "../../Data/Data";
 
 const Testimonials = () => {
   return (
@@ -6,16 +7,30 @@ const Testimonials = () => {
       <div className="text-4xl text-center font-semibold mb-3 text-mine-shaft-100">
         What <span className="text-bright-sun-400">User </span>says about us?
       </div>
-      <div>
-        <div className="flex gap-2 items-center">
-          <Avatar className="!h-14 !w-14" src={"/avatar1.png"} alt="Its me" />
-          <div>
-            <div className="text-lg text-mine-shaft-100 font-semibold">
-              Shivam Patel
-              <Rating value={3.5} fractions={2} readOnly />
+      <div className="flex justify-evenly">
+        {testimonials.map((testimonial, index) => (
+          <div
+            className="flex flex-col gap-3 w-[23%] border-bright-sun-400 p-3 border rounded-xl mt-10"
+            key={index}
+          >
+            <div className="flex gap-2 items-center">
+              <Avatar
+                className="!h-14 !w-14"
+                src={`${testimonial.avatar}.png`}
+                alt="Its me"
+              />
+              <div>
+                <div className="text-lg text-mine-shaft-100 font-semibold">
+                  {testimonial.name}
+                </div>
+                <Rating value={testimonial.rating} fractions={2} readOnly />
+              </div>
+            </div>
+            <div className="text-xs text-mine-shaft-300">
+              {testimonial.testimonial}
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
