48cefd7d2ba43913626711dc3b3bc1aa4d5c18ee
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-31 11:52:35 +0530

    Footer done and Landing Page done

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Companies from "../components/LandingPage/Companies";
 import DreamJob from "../components/LandingPage/DreamJob";
@@ -16,6 +17,7 @@ const HomePage = () => {
       <Working />
       <Testimonials />
       <Subscribe />
+      <Footer />
     </div>
   );
 };
