b19e881121cc14cb143a4261d4f45257b4e5bc57
Sanskar-12 <sanskarv2004@gmail.com>
2025-04-02 17:10:43 +0530

    Sort

---
+++ src/components/FindJobs/Sort.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Box, Button, Combobox, Text, useCombobox } from "@mantine/core";
+
+const groceries = [
+  "🍎 Apples",
+  "🍌 Bananas",
+  "🥦 Broccoli",
+  "🥕 Carrots",
+  "🍫 Chocolate",
+  "🍇 Grapes",
+];
+
+export function BasicButton() {
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+  });
+
+  const options = groceries.map((item) => (
+    <Combobox.Option value={item} key={item}>
+      {item}
+    </Combobox.Option>
+  ));
+
+  return (
+    <>
+      <Combobox
+        store={combobox}
+        width={250}
+        position="bottom-start"
+        withArrow
+        withinPortal={false}
+        onOptionSubmit={(val) => {
+          setSelectedItem(val);
+          combobox.closeDropdown();
+        }}
+      >
+        <Combobox.Target>
+          <Button onClick={() => combobox.toggleDropdown()}>Pick item</Button>
+        </Combobox.Target>
+
+        <Combobox.Dropdown>
+          <Combobox.Options>{options}</Combobox.Options>
+        </Combobox.Dropdown>
+      </Combobox>
+    </>
+  );
+}
