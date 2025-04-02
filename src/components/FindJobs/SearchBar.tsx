0ec127c1638d386bf0a706f111dc63312d881c2b
Sanskar-12 <sanskarv2004@gmail.com>
2025-04-02 11:37:41 +0530

    Slider

--- src/components/FindJobs/SearchBar.tsx
+++ src/components/FindJobs/SearchBar.tsx
@@ -1,18 +1,44 @@
+import { Divider, RangeSlider } from "@mantine/core";
 import { dropdownData } from "../../Data/JobsData";
 import { MultiSelect } from "./MultiSelect";
+import { useState } from "react";
 
 const SearchBar = () => {
+  const [value, setValue] = useState<[number, number]>([1, 100]);
+
   return (
-    <div className="flex">
+    <div className="flex px-5 py-8">
       {dropdownData.map((data, index) => (
-        <div className="w-1/5" key={index}>
-          <MultiSelect
-            title={data.title}
-            Icon={data.icon}
-            option={data.options}
-          />
-        </div>
+        <>
+          <div className="w-1/5" key={index}>
+            <MultiSelect
+              title={data.title}
+              Icon={data.icon}
+              option={data.options}
+            />
+          </div>
+          <Divider mr={"xs"} size="xs" orientation="vertical" />
+        </>
       ))}
+      <div className="w-1/5">
+        <div className="flex justify-between">
+          <div>Salary</div>
+          <div>
+            &#8377;{value[0]} - &#8377;{value[1]}
+          </div>
+        </div>
+        <RangeSlider
+          color="bright-sun.4"
+          size={"xs"}
+          value={value}
+          onChange={setValue}
+          labelTransitionProps={{
+            transition: "skew-down",
+            duration: 150,
+            timingFunction: "linear",
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/FindJobs.tsx
+++ src/pages/FindJobs.tsx
@@ -1,8 +1,10 @@
+import { Divider } from "@mantine/core";
 import SearchBar from "../components/FindJobs/SearchBar";
 
 const FindJobs = () => {
   return (
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
+      <Divider size={"xs"} mx={"md"} />
       <SearchBar />
     </div>
   );
