0ad83e2dcae294267c7bb2ded810319569c57b69
Sanskar-12 <sanskarv2004@gmail.com>
2025-04-03 10:18:31 +0530

    Sort Componenet Ui Done

--- src/components/FindJobs/Jobs.tsx
+++ src/components/FindJobs/Jobs.tsx
@@ -1,5 +1,14 @@
+import Sort from "./Sort";
+
 const Jobs = () => {
-  return <div>Jobs</div>;
+  return (
+    <div className="p-5">
+      <div className="flex justify-between">
+        <div className="text-2xl font-semibold">Recommended Jobs</div>
+        <Sort />
+      </div>
+    </div>
+  );
 };
 
 export default Jobs;
--- src/components/FindJobs/Sort.tsx
+++ src/components/FindJobs/Sort.tsx
@@ -1,23 +1,22 @@
 import { useState } from "react";
-import { Box, Button, Combobox, Text, useCombobox } from "@mantine/core";
+import { Combobox, useCombobox } from "@mantine/core";
+import { IconAdjustments } from "@tabler/icons-react";
 
-const groceries = [
-  "🍎 Apples",
-  "🍌 Bananas",
-  "🥦 Broccoli",
-  "🥕 Carrots",
-  "🍫 Chocolate",
-  "🍇 Grapes",
+const opt = [
+  "Relevance",
+  "Most Recent",
+  "Salary (Low to High)",
+  "Salary (High to Low)",
 ];
 
-export function BasicButton() {
-  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+const Sort = () => {
+  const [selectedItem, setSelectedItem] = useState<string | null>("Relevance");
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
   });
 
-  const options = groceries.map((item) => (
-    <Combobox.Option value={item} key={item}>
+  const options = opt.map((item) => (
+    <Combobox.Option value={item} key={item} className="text-xs">
       {item}
     </Combobox.Option>
   ));
@@ -26,9 +25,8 @@ export function BasicButton() {
     <>
       <Combobox
         store={combobox}
-        width={250}
+        width={150}
         position="bottom-start"
-        withArrow
         withinPortal={false}
         onOptionSubmit={(val) => {
           setSelectedItem(val);
@@ -36,7 +34,13 @@ export function BasicButton() {
         }}
       >
         <Combobox.Target>
-          <Button onClick={() => combobox.toggleDropdown()}>Pick item</Button>
+          <div
+            className="border border-bright-sun-400 flex px-2 py-1 rounded-xl items-center cursor-pointer gap-2 text-sm"
+            onClick={() => combobox.toggleDropdown()}
+          >
+            {selectedItem}{" "}
+            <IconAdjustments className="text-bright-sun-400 h-5 w-5" />
+          </div>
         </Combobox.Target>
 
         <Combobox.Dropdown>
@@ -45,4 +49,6 @@ export function BasicButton() {
       </Combobox>
     </>
   );
-}
+};
+
+export default Sort;
