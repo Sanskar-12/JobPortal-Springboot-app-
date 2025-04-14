092823fb74bf5a16e8c2a3ddfe733e9a654cd988
Sanskar-12 <sanskarv2004@gmail.com>
2025-04-14 12:43:18 +0530

    made user service and implementation

---
+++ jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.jobportal.Job.Portal.service;
+
+import com.jobportal.Job.Portal.dto.UserDTO;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDTO registerUser(UserDTO userDTO) {
+        return null;
+    }
+
+}
