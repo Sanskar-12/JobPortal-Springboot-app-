dee4a8ff05b72e74c4c652b1490016a24f867f39
Sanskar-12 <sanskarv2004@gmail.com>
2025-04-15 12:24:14 +0530

    Sequence DTO

---
+++ jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/entity/Sequence.java
@@ -0,0 +1,12 @@
+package com.jobportal.Job.Portal.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "sequence")
+public class Sequence {
+
+    @Id
+    private String id;
+    private Long seq;
+}
