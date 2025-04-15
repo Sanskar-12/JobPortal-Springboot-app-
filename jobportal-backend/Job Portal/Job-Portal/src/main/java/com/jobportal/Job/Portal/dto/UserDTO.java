f6fe4a6cd00bad87c4c3727b525f017e25342531
Sanskar-12 <sanskarv2004@gmail.com>
2025-04-15 11:56:29 +0530

    handled Exception and displayed default error message

--- jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/dto/UserDTO.java
+++ jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/dto/UserDTO.java
@@ -1,7 +1,9 @@
 package com.jobportal.Job.Portal.dto;
 
 import com.jobportal.Job.Portal.entity.User;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,8 +19,10 @@ public class UserDTO {
     @NotBlank(message = "Name is null or blank")
     private String name;
     @NotBlank(message = "Email is null or blank")
+    @Email(message = "Email is invaliduppa")
     private String email;
     @NotBlank(message = "Password is null or blank")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$\n", message = "Password is invalid")
     private String password;
     private AccountType accountType;
 
--- jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/utility/ExceptionControllerAdvice.java
+++ jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/utility/ExceptionControllerAdvice.java
@@ -1,11 +1,16 @@
 package com.jobportal.Job.Portal.utility;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
@@ -19,5 +24,26 @@ public class ExceptionControllerAdvice {
         );
 
         return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    public ResponseEntity<ErrorInfo> validatorExceptionHandler(Exception exception) {
+        String msg="";
+
+        if(exception instanceof MethodArgumentNotValidException methodArgumentNotValidException) {
+            msg = methodArgumentNotValidException.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(", "));
+        } else {
+            ConstraintViolationException constraintViolationException = (ConstraintViolationException) exception;
+
+            msg = constraintViolationException.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(", "));
+        }
+
+        ErrorInfo error = new ErrorInfo(
+                msg,
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
     }
 }
