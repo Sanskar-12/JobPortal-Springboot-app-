574ba12eae7193634e7f5b50ab1281841c134193
Sanskar-12 <sanskarv2004@gmail.com>
2025-04-15 09:31:26 +0530

    setup validation for DTO fields

--- jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/api/UserAPI.java
+++ jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/api/UserAPI.java
@@ -2,12 +2,15 @@ package com.jobportal.Job.Portal.api;
 
 import com.jobportal.Job.Portal.dto.UserDTO;
 import com.jobportal.Job.Portal.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Validated
 @CrossOrigin
 @RequestMapping("/users")
 public class UserAPI {
@@ -16,7 +19,7 @@ public class UserAPI {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<UserDTO> registerUser(@RequestBody @Valid UserDTO userDTO) {
         userDTO = userService.registerUser(userDTO);
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
     }
--- jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/dto/UserDTO.java
+++ jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.jobportal.Job.Portal.dto;
 
 import com.jobportal.Job.Portal.entity.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,11 @@ import org.springframework.data.mongodb.core.index.Indexed;
 @NoArgsConstructor
 public class UserDTO {
     private String id;
+    @NotBlank(message = "Name is null or blank")
     private String name;
+    @NotBlank(message = "Email is null or blank")
     private String email;
+    @NotBlank(message = "Password is null or blank")
     private String password;
     private AccountType accountType;
 
