bc4104593b268a3c700de13b1b7890360f36a924
Sanskar-12 <sanskarv2004@gmail.com>
2025-04-28 18:19:52 +0530

    Classes made for profile

---
+++ jobportal-backend/Job Portal/Job-Portal/src/main/java/com/jobportal/Job/Portal/entity/Profile.java
@@ -0,0 +1,29 @@
+package com.jobportal.Job.Portal.entity;
+
+import com.jobportal.Job.Portal.dto.Certifications;
+import com.jobportal.Job.Portal.dto.Experience;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "profiles")
+public class Profile {
+    @Id
+    private Long id;
+    private String email;
+    private String jobTitle;
+    private String company;
+    private String location;
+    private String about;
+    private List<String> skills;
+    private List<Experience> experience;
+    private List<Certifications> certifications;
+
+}
