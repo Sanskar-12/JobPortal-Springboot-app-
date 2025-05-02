83b3aa3175a7818a2582ce1e747e847132422cc3
Sanskar-12 <sanskarv2004@gmail.com>
2025-05-02 12:19:39 +0530

    Avatar ongoing

--- jobportal-frontend/src/components/Profile/Profile.tsx
+++ jobportal-frontend/src/components/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { Divider } from "@mantine/core";
+import { Avatar, Divider, FileInput, Indicator } from "@mantine/core";
 import { IUser, profileUserServiceType } from "../../types";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,6 +10,7 @@ import About from "./About";
 import Skills from "./Skills";
 import Experience from "./Experience";
 import Certifications from "./Certifications";
+import { IconPencil } from "@tabler/icons-react";
 
 const Profile = () => {
   const user = useSelector((state: IRootUserState) => state.user) as IUser;
@@ -17,15 +18,7 @@ const Profile = () => {
     (state: IRootUserState) => state.profile
   ) as profileUserServiceType;
 
-  // const [edit, setEdit] = useState([false, false, false, false, false]);
-
   const dispatch = useDispatch();
-
-  // const handleEdit = (index: number) => {
-  //   const newData = [...edit];
-  //   newData[index] = !newData[index];
-  //   setEdit(newData);
-  // };
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -43,11 +36,32 @@ const Profile = () => {
     <div className="w-4/5 mx-auto">
       <div className="relative">
         <img src="/Profile/banner.jpg" alt="Banner" className="rounded-t-2xl" />
-        <img
-          src="/avatar.png"
-          alt="Avatar"
-          className="w-48 h-48 rounded-full -bottom-1/3 absolute left-3 border-mine-shaft-950 border-8"
-        />
+        <div className="absolute -bottom-1/3 left-3">
+          <Indicator
+            className="[&_.mantine-Indicator-indicator]:!border-4 [&_img]:hover:opacity-80"
+            autoContrast
+            inline
+            offset={30}
+            label={<IconPencil className="w-4/5 h-4/5" />}
+            size={45}
+            position="bottom-end"
+            color="bright-sun.4"
+            withBorder
+          >
+            <Avatar
+              className="!w-48 !h-48 border-mine-shaft-950 border-8 rounded-full"
+              src={"/avatar.png"}
+              alt="Profile Picture"
+            />
+            <FileInput
+              className="absolute bottom-2 right-2 z-[201] w-12 [&_div]:text-transparent"
+              variant="unstyled"
+              size="lg"
+              radius={"xl"}
+              accept="image/png,image/jpeg"
+            />
+          </Indicator>
+        </div>
       </div>
       <div className="px-3 mt-28">
         <Info profile={profile} />
