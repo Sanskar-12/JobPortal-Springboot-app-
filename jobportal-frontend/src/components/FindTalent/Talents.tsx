2d8885971ca6a7bdcfd9822cbca7b7f88603ad86
Sanskar-12 <sanskarv2004@gmail.com>
2025-06-07 11:21:31 +0530

    talent page fixed

--- jobportal-frontend/src/components/FindTalent/Talents.tsx
+++ jobportal-frontend/src/components/FindTalent/Talents.tsx
@@ -1,8 +1,27 @@
-import { talents } from "../../Data/TalentData";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
 import Sort from "../FindJobs/Sort";
 import TalentCard from "../FindJobs/TalentCard";
+import { errorNotification } from "../../Services/NotificationService";
+import { getAllProfile } from "../../Services/ProfileService";
+import { profileUserServiceType } from "../../types";
 
 const Talents = () => {
+  const [talents, setTalents] = useState<profileUserServiceType[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getAllProfile();
+        setTalents(res);
+      } catch (error: any) {
+        console.log(error);
+        errorNotification("Error", error?.reponse.data.errorMessage);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="p-5">
       <div className="flex justify-between">
@@ -11,17 +30,7 @@ const Talents = () => {
       </div>
       <div className="mt-10 grid grid-cols-4 gap-5">
         {talents.map((talent, index) => (
-          <TalentCard
-            key={index}
-            name={talent.name}
-            role={talent.role}
-            company={talent.company}
-            topSkills={talent.topSkills}
-            about={talent.about}
-            expectedCtc={talent.expectedCtc}
-            location={talent.location}
-            image={talent.image}
-          />
+          <TalentCard key={index} applicantId={talent?.id.toString()} />
         ))}
       </div>
     </div>
