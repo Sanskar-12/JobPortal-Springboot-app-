8988593361e7e66be0df79eec5d0c65b39ce6f50
Sanskar-12 <sanskarv2004@gmail.com>
2025-06-08 17:12:48 +0530

    dispatch functionality done in Filters

--- jobportal-frontend/src/components/FindTalent/FindTalentSearchBar.tsx
+++ jobportal-frontend/src/components/FindTalent/FindTalentSearchBar.tsx
@@ -1,11 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Divider, Input, RangeSlider } from "@mantine/core";
 import { useState } from "react";
 import { MultiSelect } from "../FindJobs/MultiSelect";
 import { searchFields } from "../../Data/TalentData";
 import { IconUserCircle } from "@tabler/icons-react";
+import { useDispatch } from "react-redux";
+import { updateFilter } from "../../redux/Slice/filterSlice";
 
 const FindTalentSearchBar = () => {
-  const [value, setValue] = useState<[number, number]>([1, 100]);
+  const [value, setValue] = useState<[number, number]>([1, 50]);
+  const [name, setName] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleChange = (name: string, e: any) => {
+    if (name === "exp") {
+      dispatch(
+        updateFilter({
+          [name]: e,
+        })
+      );
+    } else {
+      setName(e.target.value);
+      dispatch(
+        updateFilter({
+          [name]: e.target.value,
+        })
+      );
+    }
+  };
 
   return (
     <div className="flex px-5 py-8 items-center !text-mine-shaft-100">
@@ -17,6 +40,8 @@ const FindTalentSearchBar = () => {
           className="[&_input]:!placeholder-mine-shaft-300"
           variant="unstyled"
           placeholder="Talent Name"
+          defaultValue={name}
+          onChange={(e) => handleChange("name", e)}
         />
       </div>
       {searchFields.map((data, index) => (
@@ -33,16 +58,19 @@ const FindTalentSearchBar = () => {
       ))}
       <div className="w-1/5 [&_.mantine-Slider-label]:!translate-y-10">
         <div className="flex justify-between text-sm">
-          <div>Salary</div>
+          <div>Experience (Years)</div>
           <div>
-            &#8377;{value[0]} LPA - &#8377;{value[1]} LPA
+            {value[0]} - {value[1]}
           </div>
         </div>
         <RangeSlider
           color="bright-sun.4"
           size={"xs"}
+          max={50}
+          min={1}
           value={value}
           onChange={setValue}
+          onChangeEnd={(e) => handleChange("exp", e)}
           labelTransitionProps={{
             transition: "skew-down",
             duration: 150,
